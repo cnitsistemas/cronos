80fc151d62456f6009fa3ac253b91a8ef63602ad
João Paulo Franco da Cruz <joaofranco.cruz@gmail.com>
2023-03-31 09:52:39 -0300

    Adicionando contador de usuarios ao dashbord

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\AppBaseController;
 use App\Repositories\AlunosRepository;
 use App\Repositories\RotasRepository;
+use App\Repositories\UserRepository;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -12,11 +13,13 @@ class DashboardController extends AppBaseController
 {
     private AlunosRepository $alunosRepository;
     private RotasRepository $rotasRepository;
+    private UserRepository $usersRepository;
 
-    public function __construct(AlunosRepository $alunosRepo, RotasRepository $rotasRepo)
+    public function __construct(AlunosRepository $alunosRepo, RotasRepository $rotasRepo, UserRepository $usersRepo)
     {
         $this->alunosRepository = $alunosRepo;
         $this->rotasRepository = $rotasRepo;
+        $this->usersRepository = $usersRepo;
     }
 
     /**
@@ -28,12 +31,14 @@ class DashboardController extends AppBaseController
     {
         $countStudents = $this->alunosRepository->model()::count();
         $countRotas = $this->rotasRepository->model()::count();
+        $countUsers = $this->usersRepository->model()::count();
         $lastStudents = $this->alunosRepository->model()::with(['route'])->orderBy('id', 'desc')->take(4)->get();
 
         $lastRoutes = $this->rotasRepository->model()::orderBy('id', 'desc')->take(4)->get();
 
         $data['totalAlunos'] = $countStudents;
         $data['totalRotas'] = $countRotas;
+        $data['totalUsuarios'] = $countUsers;
         $data['alunos'] = $lastStudents;
         $data['rotas'] = $lastRoutes;
 
